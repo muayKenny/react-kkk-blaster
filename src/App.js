c8d746ee9d63f441297e5b548541720dc6ed15ba
Kenny Sheftel <kenshef@kennys-air.home>
2020-08-27 12:12:30 -0700

    removed history, custom project begins

---
+++ src/App.js
@@ -0,0 +1,145 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
+import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+import io from 'socket.io-client';
+
+if (process.env.NODE_ENV === 'production') {
+  auth0_uri = process.env.REACT_APP_AUTH0_AUTHURI;
+} else {
+  auth0_uri = 'http://localhost:5000'
+}
+
+Auth0.configure({
+  domain: process.env.REACT_APP_AUTH0_DOMAIN,
+  clientID: process.env.REACT_APP_AUTH0_CLIENTID,
+  redirectUri: auth0_uri,
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.shoot = this.shoot.bind(this);
+    this.socket = null;
+    this.currentPlayer = null
+  }
+
+  componentDidMount() {
+    const self = this;
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe((auth) => {
+      if (!auth) return;
+      self.playerProfile = Auth0.getProfile();
+      self.currentPlayer = {
+        id: self.playerProfile.sub,
+        maxScore: 0,
+        name: self.playerProfile.name,
+        picture: self.playerProfile.picture,
+      };
+
+      this.props.loggedIn(self.currentPlayer);
+      self.socket = io('http://localhost:3001', {
+        query: `token=${Auth0.getAccessToken()}`,
+      });
+
+      self.socket.on('players', (players) => {
+        this.props.leaderboardLoaded(players);
+        players.forEach((player) => {
+          if (player.id === self.currentPlayer.id) {
+            self.currentPlayer.maxScore = player.maxScore;
+          }
+        });
+      });
+    });
+
+    setInterval(() => {
+        self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+
+    window.onresize = () => {
+      const cnv = document.getElementById('aliens-go-home-canvas');
+      cnv.style.width = `${window.innerWidth}px`;
+      cnv.style.height = `${window.innerHeight}px`;
+    };
+    window.onresize();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.gameState.started && this.props.gameState.started) {
+      if (this.currentPlayer.maxScore < this.props.gameState.kills) {
+        this.socket.emit('new-max-score', {
+          ...this.currentPlayer,
+          maxScore: this.props.gameState.kills,
+        });
+      }
+    }
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
+  shoot() {
+    this.props.shoot(this.canvasMousePosition);
+  }
+
+  render() {
+    return (
+      <Canvas
+        angle={this.props.angle}
+        gameState={this.props.gameState}
+        startGame={this.props.startGame}
+        players={this.props.players}
+        currentPlayer={this.props.currentPlayer}
+        trackMouse={event => (this.trackMouse(event))}
+        shoot={this.shoot}
+      />
+    );
+  }
+}
+
+App.propTypes = {
+  angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+      position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired,
+      id: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
+  moveObjects: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }),
+  leaderboardLoaded: PropTypes.func.isRequired,
+  loggedIn: PropTypes.func.isRequired,
+  players: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  })),
+  shoot: PropTypes.func.isRequired,
+};
+
+
+App.defaultProps = {
+  currentPlayer: null,
+  players: null,
+};
+
+export default App;
